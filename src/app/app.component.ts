69e3ddd885a4b265a95ec5f69c2f43bbce5b42f8
kiran <kiran.jakka@acknotech.com>
2017-08-19 19:15:01 +0530

    Added routing and cards in card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <nav>
+      <a routerLink="/card" routerLinkActive="active">Card</a>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My First Angular app';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from "@angular/material";
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+import "hammerjs";
+
+const routes: Routes = [
+  // { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'dashboard',  component: DashboardComponent },
+  { path: 'card',  component: CardComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    DashboardComponent,
+    SidenavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes),BrowserAnimationsModule,MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
